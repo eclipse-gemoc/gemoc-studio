2d4d2e9cd0c0858c907803ae1d8256e7c16eaf6e
Didier Vojtisek <didier.vojtisek@inria.fr>
2018-08-16 16:00:49 +0200

    add gtk3 to docker image
    Signed-off-by: Didier Vojtisek <didier.vojtisek@inria.fr>

--- dev_support/full_compilation/docker/entrypoint.sh
+++ dev_support/full_compilation/docker/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+Xvfb :99 &
+export DISPLAY=:99
+
 # compile full gemoc studio
-xvfb-run mvn -f /root/src/gemoc-studio/dev_support/full_compilation/pom.xml clean verify --errors -X
+mvn -f /root/src/gemoc-studio/dev_support/full_compilation/pom.xml clean verify --errors -X
 
 # set owner to default system user
 chown 1000:1000 -R /root/src/gemoc-studio
