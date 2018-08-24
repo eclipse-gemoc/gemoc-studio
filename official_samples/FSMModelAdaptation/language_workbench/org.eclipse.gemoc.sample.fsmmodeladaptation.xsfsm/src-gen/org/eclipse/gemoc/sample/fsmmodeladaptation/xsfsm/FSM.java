56ef14d7407b8688d9382b689ef292c43ec6ea5e
Didier Vojtisek <didier.vojtisek@inria.fr>
2018-08-24 13:56:52 +0200

    new test (currently ignored) about the end of the execution
    see https://github.com/eclipse/gemoc-studio-modeldebugging/issues/66
    
    Signed-off-by: Didier Vojtisek <didier.vojtisek@inria.fr>

---
+++ official_samples/FSMModelAdaptation/language_workbench/org.eclipse.gemoc.sample.fsmmodeladaptation.xsfsm/src-gen/org/eclipse/gemoc/sample/fsmmodeladaptation/xsfsm/FSM.java
@@ -0,0 +1,28 @@
+package org.eclipse.gemoc.sample.fsmmodeladaptation.xsfsm;
+
+import fr.inria.diverse.melange.lib.IMetamodel;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+
+@SuppressWarnings("all")
+public class FSM implements IMetamodel {
+  private Resource resource;
+  
+  public Resource getResource() {
+    return this.resource;
+  }
+  
+  public void setResource(final Resource resource) {
+    this.resource = resource;
+  }
+  
+  public static FSM load(final String uri) {
+    ResourceSet rs = new ResourceSetImpl();
+    Resource res = rs.getResource(URI.createURI(uri), true);
+    FSM mm = new FSM();
+    mm.setResource(res);
+    return mm ;
+  }
+}
