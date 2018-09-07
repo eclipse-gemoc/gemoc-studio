1c711240c50968fb04147f896c7cc01bfbb7633b
Didier Vojtisek <didier.vojtisek@inria.fr>
2018-09-07 09:40:45 +0200

    adapt compliance settings to target compiler (java8)
    Signed-off-by: Didier Vojtisek <didier.vojtisek@inria.fr>

--- official_samples/K3FSM/language_workbench/org.eclipse.gemoc.example.k3fsm.design/src/org/eclipse/gemoc/example/k3fsm/design/services/FsmAnimatorServices.java
+++ official_samples/K3FSM/language_workbench/org.eclipse.gemoc.example.k3fsm.design/src/org/eclipse/gemoc/example/k3fsm/design/services/FsmAnimatorServices.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Inria.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Inria - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.gemoc.example.k3fsm.design.services;
 
 import java.util.ArrayList;
--- official_samples/K3FSM/language_workbench/org.eclipse.gemoc.example.k3fsm.design/src/org/eclipse/gemoc/example/k3fsm/design/services/FsmDebugServices.java
+++ official_samples/K3FSM/language_workbench/org.eclipse.gemoc.example.k3fsm.design/src/org/eclipse/gemoc/example/k3fsm/design/services/FsmDebugServices.java
@@ -1,4 +1,14 @@
-﻿package org.eclipse.gemoc.example.k3fsm.design.services;
+﻿/*******************************************************************************
+ * Copyright (c) 2018 Inria.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Inria - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.gemoc.example.k3fsm.design.services;
 
 import java.util.ArrayList;
 import java.util.List;
