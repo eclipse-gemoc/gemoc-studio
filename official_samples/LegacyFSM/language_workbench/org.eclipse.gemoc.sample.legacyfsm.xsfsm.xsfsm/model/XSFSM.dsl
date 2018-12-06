c185a8464a00b0cf52b1c6bfdf524384ac213e20
Didier Vojtisek <didier.vojtisek@inria.fr>
2018-12-06 09:14:37 +0100

    Move dsl default location to project root (#124)
    * bump melange to 2018-11-29
    
    this contain additional support for https://github.com/eclipse/gemoc-studio-modeldebugging/issues/65
    
    * Update CreateMelangeBasedSingleSequentialLanguageFromOfficialFSM_Test.xtend
    * regenerate example using current version
    
    * hopefully more robust swttest
    
    * move dsl file to root of the sample project
    
    * fix some warnings in the example
    
    * fix autoregister nature id in example
    
    Signed-off-by: Didier Vojtisek <didier.vojtisek@inria.fr>

--- official_samples/LegacyFSM/language_workbench/org.eclipse.gemoc.sample.legacyfsm.xsfsm.xsfsm/model/XSFSM.dsl
+++
@@ -1,3 +0,0 @@
-name = org.eclipse.gemoc.sample.legacyfsm.xsfsm.XSFSM
-ecore = platform:/resource/org.eclipse.gemoc.sample.legacyfsm.xsfsm.xsfsm/model/XSFSM.ecore
-k3 = org.eclipse.gemoc.sample.legacyfsm.xsfsm.xsfsm.aspects.StateAspect,org.eclipse.gemoc.sample.legacyfsm.xsfsm.xsfsm.aspects.StateMachineAspect,org.eclipse.gemoc.sample.legacyfsm.xsfsm.xsfsm.aspects.TransitionAspect
--- official_samples/LegacyFSM/language_workbench/org.eclipse.gemoc.sample.legacyfsm.xsfsm.xsfsm/src/org/eclipse/gemoc/sample/legacyfsm/xsfsm/xsfsm/fsm/impl/TransitionImpl.java
+++ official_samples/LegacyFSM/language_workbench/org.eclipse.gemoc.sample.legacyfsm.xsfsm.xsfsm/src/org/eclipse/gemoc/sample/legacyfsm/xsfsm/xsfsm/fsm/impl/TransitionImpl.java
@@ -418,7 +418,7 @@ public class TransitionImpl extends NamedElementImpl implements Transition {
 	public String toString() {
 		if (eIsProxy()) return super.toString();
 
-		StringBuffer result = new StringBuffer(super.toString());
+		StringBuilder result = new StringBuilder(super.toString());
 		result.append(" (input: ");
 		result.append(input);
 		result.append(", output: ");
